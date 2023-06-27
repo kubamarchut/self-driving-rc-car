48979723145b42d0c579f8df545750cc80670f91
Kuba Marchut <kuba.marchut@gmail.com>
2023-06-27 10:20:49 +0200

    applying same distance measeuring method to standalone measuring script from distance measuring class

--- sonar_sys.js
+++ sonar_sys.js
@@ -1,47 +1,48 @@
-function avg(arr){
+function avg(arr) {
   let sum = 0;
-  for(let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i]
   }
   return sum / arr.length
 }
 
 const Gpio = require('pigpio').Gpio;
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6/34321;
+// The number of microseconds it takes sound to travel 1cm at 20 degrees celsius
+const MICROSECONDS_PER_CM = 1e6 / 34321;
 
-const trigger = new Gpio(20, {mode: Gpio.OUTPUT});
-let echo = new Gpio(21, {mode: Gpio.INPUT, alert: true});
+const trigger = new Gpio(20, { mode: Gpio.OUTPUT });
+let echo = new Gpio(21, { mode: Gpio.INPUT, alert: true });
 
 trigger.digitalWrite(0); // Make sure trigger is low
 
 let distances = new Array(3).fill(0);
 
+let echoDetected = true;
 // Trigger a distance measurement once per second{mode: Gpio.OUTPUT}
 let triggering = setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+  if (echoDetected) {
+    trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+    echoDetected = false;
+  }
 }, 100);
 
 let old_distance = 0, distance = 0;
 
 echo.on('alert', (level, tick) => {
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      old_distance = distance
-      distance = (diff / 2 / MICROSECDONDS_PER_CM);
-      c_dis = distance;
-      if(distance > 1 && distance < 1600){
-        distance = distance
-      }
-      else{
-        distance = 400;
-      }
-      distances.shift();
-      distances.push(distance);
-      distance = avg(distances)
-      console.log(c_dis.toFixed(2), distance.toFixed(2));
+  if (level == 1) {
+    startTick = tick;
+  } else {
+    echoDetected = true;
+    const endTick = tick;
+    const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+
+    let distance = (diff / 2 / MICROSECONDS_PER_CM);
+    if (distance < 2 || distance > 400) {
+      distance = Infinity;
     }
+    distances.shift();
+    distances.push(distance);
+
+    console.log(distances[2].toFixed(2), avg(distances).toFixed(2))
+  }
 });
